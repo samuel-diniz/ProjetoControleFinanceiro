3ea4dce7287c43f98ff3b3a0a537b94985ebd0ab
Samuel Diniz de Carvalho <samuca0711@gmail.com>
2026-02-09 09:16:11 -0300

    Corrige contrato de dados e finaliza fluxo de gastos parcelados

--- main.py
+++ main.py
@@ -5,43 +5,37 @@ from dateutil.relativedelta import relativedelta
 from config import arquivo_excel
 
 
-def main():
-    wb = carregar_planilha()
-
-    aba = escolher_aba()
-    if aba is None:
-        print("Encerrando o programa.")
-        return
-    
-    elif aba == 'E':
-        print("Você escolheu registrar uma nova ENTRADA.")
-        dados = {
-            'data': pedir_data(),
-            'descricao': pedir_texto('Descrição: '),
-            'categoria': pedir_categoria(),
-            'valor': pedir_valor('Valor: '),
-            'forma_pagamento': pedir_forma_pagamento()['tipo']
-        }   
-        salvar_entrada(wb, dados)
-        wb.save(arquivo_excel)
+def registrar_entrada(wb):
+    print("Você escolheu registrar uma nova ENTRADA.")
         
-        print("Entrada registrada com sucesso!")
+    dados = {
+        'data': pedir_data(),
+        'descricao': pedir_texto('Descrição: '),
+        'categoria': pedir_categoria(),
+        'valor': pedir_valor('Valor: '),
+        'forma_de_pagamento': pedir_forma_pagamento()['tipo']
+    }   
+    salvar_entrada(wb, dados)
+    wb.save(arquivo_excel)
         
-    elif aba == 'G':
-        print("Você escolheu registrar um novo GASTO.")
-        data = pedir_data()
-        descricao = pedir_texto('Descrição: ')
-        categoria = pedir_categoria()
-        valor = pedir_valor('Valor: ')
-        forma_pagamento = pedir_forma_pagamento()
+    print("Entrada registrada com sucesso!")
 
-        if forma_pagamento['parcelado']:
-            num_parcelas = forma_pagamento['numero_parcelas']
-            valor_parcela = round(valor / num_parcelas, 2)
 
-            for i in range(num_parcelas):
+def registrar_gasto(wb):
+    print("Você escolheu registrar um novo GASTO.")
+    data = pedir_data()
+    descricao = pedir_texto('Descrição: ')
+    categoria = pedir_categoria()
+    valor = pedir_valor('Valor: ')
+    forma_pagamento = pedir_forma_pagamento()
 
-                salvar_gastos(wb, {
+    if forma_pagamento['parcelado']:
+        num_parcelas = forma_pagamento['numero_parcelas']
+        valor_parcela = round(valor / num_parcelas, 2)
+
+        for i in range(num_parcelas):
+
+            salvar_gastos(wb, {
                 'data': data + relativedelta(months=i),
                 'descricao': f"{descricao} ({i+1}/{num_parcelas})",
                 'categoria': categoria,
@@ -51,19 +45,36 @@ def main():
                 'numero_parcelas': num_parcelas,
                 'valor_parcela': valor_parcela
         })
-        else:
-            salvar_gastos(wb, {
-                'data': data,
-                'descricao': descricao,
-                'categoria': categoria,
-                'valor_total': valor,
-                'forma_pagamento': forma_pagamento['tipo'],
-                'parcelado': False,
-                'numero_parcelas': None,
-                'valor_parcela': None
-            })
-        wb.save(arquivo_excel)
-        print('Gasto registrado com sucesso!')
+
+            
+    else:
+        salvar_gastos(wb, {
+            'data': data,
+            'descricao': descricao,
+            'categoria': categoria,
+            'valor_total': valor,
+            'forma_de_pagamento': forma_pagamento['tipo'],
+            'parcelado': False,
+            'numero_parcelas': None,
+            'valor_parcela': None
+        })
+    wb.save(arquivo_excel)
+    print('Gasto registrado com sucesso!')
+
+
+def main():
+    wb = carregar_planilha()
+    aba = escolher_aba()
+    
+    if aba is None:
+        print("Encerrando o programa.")
+        return
+    
+    elif aba == 'E':
+        registrar_entrada(wb)
+        
+    elif aba == 'G':
+        registrar_gasto(wb)
 
 if __name__ == "__main__":
     main()
